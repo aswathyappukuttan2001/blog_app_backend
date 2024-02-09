aa9245923444076f0e39fbdfd23edcb146fa6ea6
aswathyappukuttan2001 <aswathyappukuttan2019>
2024-02-09 11:31:17 +0530

    securityadd

--- controller/userRouter.js
+++ controller/userRouter.js
@@ -1,17 +1,34 @@
 const express=require("express")
 const userModel=require("../model/userModel")
-
 const router=express.Router()
+const bcrypt=require("bcryptjs")
 
+
+hashpasswordGenerator=async(pass)=>{    //encryption password
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
 router.post("/add",async(req,res)=>{
-    let data=req.body
-    let blog=new userModel(data)
-    let result=await blog.save()
-    res.json(
-        {
-            status:"success"
+    let {data}={"data":req.body}
+    let password=data.password
+    hashpasswordGenerator(password).then(
+        (hashedpassword)=>{
+            console.log(hashedpassword)
+            data.password=hashedpassword
+            console.log(data)
+     let blog=new userModel(data)
+     let result=blog.save()
+     res.json(
+         {
+             status:"success"
+         }
+     )
         }
     )
+     
+    //console.log(data)
+
+
 })
 
    
